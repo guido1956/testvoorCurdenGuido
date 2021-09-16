538b7359953fac1fc463792a682b0f8bb02fba67
Guido22 <GuidoDulos@valori.nl>
2021-09-16 12:56:45 +0200

    Versie 2 subtract method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,6 +5,6 @@ public class Calculator {
     }
 
     public int subtract (int a, int b) {
-        return a - b; // guido22 aangepast
+        return a - b; // guido22 aangepast voor pulrequest
     }
 }
